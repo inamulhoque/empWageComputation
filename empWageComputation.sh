637c50132aab5d454c6c44bde7f1e12b857cc16f
Inamul <inamulhoque097@gmail.com>
2022-05-11 23:38:07 +0530

    Update: Calculating Wages for a Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,17 +1,21 @@
 #!/bin/bash -x
 
-isPartTime=0
-isFullTime=1
+isPartTime=1
+isFullTime=2
+totalSalary=0
 EMP_RATE_PER_HR=20
+NUM_WORKING_DAYS=20
 
-rand=$(($RANDOM%3))
-case $rand in
-	$isFullTime) empHrs=8;;
-	$isPartTime) empHrs=4;;
-	*) empHrs=0;;
-esac
+for (( day=1; day<=$NUM_WORKING_DAYS; day++ ))
+do
+	rand=$(($RANDOM%3))
+	case $rand in
+		$isFullTime) empHrs=8;;
+		$isPartTime) empHrs=4;;
+		*) empHrs=0;;
+	esac
 
 salary=$(($empHrs*$EMP_RATE_PER_HR))
-echo "Salary: $salary"
-
-
+totalSalary=$(($totalSalary+$salary))
+echo "Salary: $totalSalary"
+done
