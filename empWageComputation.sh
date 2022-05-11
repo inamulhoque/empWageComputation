de9a91a455d4d89d312decc9fbb5c6155071d4c6
Inamul <inamulhoque097@gmail.com>
2022-05-11 21:54:33 +0530

    Update: Calculation of daily Employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,10 +2,28 @@
 
 echo "Welcome to Employee Wage Computation Program"
 
+isPartTime=0
+isFullTime=1
+EMP_RATE_PER_HR=20
+
 rand=$(($RANDOM%2))
 if [ $rand -eq 1 ]
 then
-echo "Employee is present"
+	echo "Employee is present"
+	check=$(($RANDOM%2))
+	if [ $check -eq $isFullTime ]
+	then
+		empHrs=8
+		salary=$(($empHrs*$EMP_RATE_PER_HR))
+		echo "Full time employee salary: $salary"
+	elif [ $check -eq $isPartTime ]
+	then
+		empHrs=4
+		salary=$(($empHrs*$EMP_RATE_PER_HR))
+		echo "Part time employee salary: $salary"
+	fi
 else
-echo "Employee is absent"
+	echo "Employee is absent"
 fi
+
+
